8f579e3880515b4a8125da3a161a42dac6b4bc3a
Norberto <rhamudjam@gmail.com>
2025-08-21 09:08:03 -0800

    Instanciamento do repositório band

--- app/Services/FrontService.php
+++ app/Services/FrontService.php
@@ -5,17 +5,21 @@ namespace App\Services;
 use App\Models\Vehicle;
 use App\Contracts\VehicleModelRepositoryInterface;
 use App\Contracts\VehicleRepositoryInterface;
+use App\Repositories\BrandRepository;
 
 class FrontService{
 
     protected $vehicleModelRepository;
     protected $vehicleRepository;
+    protected $brandRepository;
 
-    public function __construct(VehicleRepositoryInterface $vehicleRepository, VehicleModelRepositoryInterface $vehicleModelRepository){
+    public function __construct(VehicleRepositoryInterface $vehicleRepository, VehicleModelRepositoryInterface $vehicleModelRepository, BrandRepository $brandRepository){
 
         $this->vehicleModelRepository = $vehicleModelRepository;
         $this->vehicleRepository = $vehicleRepository;
-        
+        $this->brandRepository = $brandRepository;
+
+
     }
 
     public function searchVehicle(int $id){
@@ -25,8 +29,9 @@ class FrontService{
     public function getFrontPageData(){
         $vehicleModels = $this->vehicleModelRepository->getActive();
         $vehicles = $this->vehicleRepository->getAllVehicles();
+        $brand = $this->brandRepository->getActive();
         // $vehicle = $this->vehicleRepository->find('id');
 
-        return compact('vehicleModels', 'vehicles');
+        return compact('vehicleModels', 'vehicles', 'brand');
     }
 }
